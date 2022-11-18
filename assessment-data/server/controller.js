1f55f501355213317abf9ff294554b4fa5c00cfc
Alyssa <alyssa.black141@gmail.com>
2022-11-18 12:00:20 -0700

    finished, plus extra credit

--- assessment-data/server/controller.js
+++ assessment-data/server/controller.js
@@ -1,4 +1,16 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
 
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
     seed: (req, res) => {
@@ -11,7 +23,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities (
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR(20) ,
+                rating INT,
+                country_id INT REFERENCES countries(country_id)
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
@@ -209,9 +226,82 @@ module.exports = {
             ('Yemen'),
             ('Zambia'),
             ('Zimbabwe');
-        `).then(() => {
+        
+        INSERT INTO cities (name, rating, country_id)
+            VALUES (
+                'Vilnius',
+                3,
+                (SELECT country_id FROM countries WHERE name = 'Lithuania')
+            );
+        
+        INSERT INTO cities (name, rating, country_id)
+            VALUES (
+                'Nur-Sultan',
+                2,
+                (SELECT country_id FROM countries WHERE name = 'Kazakhstan')
+            );
+        
+        INSERT INTO cities (name, rating, country_id)
+            VALUES (
+                'Valencia',
+                4,
+                (SELECT country_id FROM countries WHERE name = 'Spain')
+            );
+        `)
+        
+        .then(() => {
             console.log('DB seeded!')
             res.sendStatus(200)
-        }).catch(err => console.log('error seeding DB', err))
+        })
+        .catch(err => console.log('error seeding DB', err))
+    },
+
+    getCountries: (req, res) => {
+        sequelize.query(`
+        SELECT * FROM countries
+        `)
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    createCity: (req, res) => {
+        let {name, rating, countryId} = req.body
+        sequelize.query(`
+        INSERT INTO cities (name, rating, country_id)
+            VALUES (
+                '${name}',
+                ${rating},
+                ${countryId}
+            );
+        `)
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    getCities: (req, res) => {
+        console.log('GETTING CITIES')
+        sequelize.query(`
+        SELECT cit.city_id, 
+        cit.name AS city, 
+        cit.rating, 
+        cit.country_id,
+        count.country_id,
+        count.name AS country
+        FROM countries AS count
+            JOIN cities AS cit
+                ON count.country_id = cit.country_id
+        ORDER BY rating DESC;
+        `)
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    deleteCity: (req, res) => {
+        let {id} = req.params
+        sequelize.query(`
+        DELETE FROM cities WHERE cities.city_id = ${id}
+        `)
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
     }
 }
